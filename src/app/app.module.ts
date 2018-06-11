9aef18c55a3aa8337b8008d7b7b50070b9dee324
Pablo Ibanez <me@pabloibanez.com>
2018-06-11 19:48:24 +0100

    Several sections added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './sections/footer/footer.component';
+import { ProfileComponent } from './sections/profile/profile.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ProfileComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
