d5c667d8cd53d9f3b163ed7636f2f143c8a5593f
pabloibanezcom <pablo.ibanez@bynd.com>
2019-03-27 12:01:46 +0000

    Google Analytics track added

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import 'font-awesome/css/font-awesome.min.css';
 import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import ReactGA from 'react-ga';
 import App from './App';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 window.jQuery = $;
 window.$ = $;
 global.jQuery = $;
 
-ReactDOM.render(<App />,document.getElementById('root'));
+if (process.env.REACT_APP_GOOGLE_ANALYTICS_ID) {
+  ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
 
